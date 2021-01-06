c2ec407c006c76983a205216faf927dc5e8d6711
Rahul Sahay <rahulsahay19@hotmail.com>
2021-01-06 17:56:00 +0530

    started Identity implementation

---
+++ BookMyEvent.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BookMyEvent.Web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
